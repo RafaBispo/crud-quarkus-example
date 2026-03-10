8e9b167feaaf4453d04374859c72f28c9cf027a9
Rafael Oliveira <rboliveira@MacBook-Air-de-Rafael.local>
2026-03-10 19:08:09 -0300

    Commit com as funcionalidades do CRUD.

---
+++ src/main/java/com/rboliveira/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.rboliveira.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+}
